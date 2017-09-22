07b4b91e85b7b68089dbac1ab51aa765ddfeb7f6
Hardy Wang <hardywang@outlook.com>
2017-09-21 20:43:26 -0400

    Wire PRISM with the application to use MVVM pattern.

--- HardySoft.GpsTracker/MainPage.xaml.cs
+++ HardySoft.GpsTracker/MainPage.xaml.cs
@@ -1,20 +1,8 @@
 ﻿namespace HardySoft.GpsTracker
 {
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
-    using System.Runtime.InteropServices.WindowsRuntime;
     using HardySoft.GpsTracker.Models;
-    using Windows.Foundation;
-    using Windows.Foundation.Collections;
-    using Windows.UI.Xaml;
+    using HardySoft.GpsTracker.ViewModels;
     using Windows.UI.Xaml.Controls;
-    using Windows.UI.Xaml.Controls.Primitives;
-    using Windows.UI.Xaml.Data;
-    using Windows.UI.Xaml.Input;
-    using Windows.UI.Xaml.Media;
-    using Windows.UI.Xaml.Navigation;
 
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -28,8 +16,7 @@
         {
             this.InitializeComponent();
 
-            this.hamburgerMenuControl.ItemsSource = MenuItem.GetMainItems();
-            this.hamburgerMenuControl.OptionsItemsSource = MenuItem.GetOptionsItems();
+            this.DataContext = new MainPageViewModel();
         }
 
         /// <summary>
