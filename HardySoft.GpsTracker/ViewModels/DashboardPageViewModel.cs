27b0882ed882e1eb5f469c62c7e2e3c46fabf202
Hardy Wang <hardywang@outlook.com>
2017-09-27 21:58:07 -0400

    Place the start/pause/stop buttons into the proper positions.

--- HardySoft.GpsTracker/ViewModels/DashboardPageViewModel.cs
+++ HardySoft.GpsTracker/ViewModels/DashboardPageViewModel.cs
@@ -7,14 +7,24 @@
     using System.Threading.Tasks;
     using HardySoft.GpsTracker.Models;
     using HardySoft.GpsTracker.Services.Gpx;
+    using HardySoft.GpsTracker.Services.Models;
     using Prism.Mvvm;
+    using Windows.UI.Xaml.Controls;
 
     /// <summary>
     /// A view model for dashboard page.
     /// </summary>
     public class DashboardPageViewModel : BindableBase
     {
+        /// <summary>
+        /// A GPX handler implementation.
+        /// </summary>
         private readonly IGpxHandler gpxHandler;
+
+        /// <summary>
+        /// The status of the tracking.
+        /// </summary>
+        private TrackingStatus status;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="DashboardPageViewModel"/> class.
@@ -23,11 +33,60 @@
         public DashboardPageViewModel(IGpxHandler gpxHandler)
         {
             this.gpxHandler = gpxHandler ?? throw new ArgumentNullException(nameof(gpxHandler));
+
+            this.status = TrackingStatus.Stopped;
         }
 
         /// <summary>
         /// Gets a list of supported activity types.
         /// </summary>
         public IEnumerable<ActivityTypes> SupportedActivityTypes => Enum.GetValues(typeof(ActivityTypes)).Cast<ActivityTypes>();
+
+        /// <summary>
+        /// Gets the icon for the start/pause button.
+        /// </summary>
+        public Symbol StartPauseButtonIcon
+        {
+            get
+            {
+                switch (this.status)
+                {
+                    case TrackingStatus.Started:
+                        return Symbol.Pause;
+                    case TrackingStatus.Paused:
+                        return Symbol.Play;
+                    case TrackingStatus.Stopped:
+                        return Symbol.Play;
+                    default:
+                        throw new InvalidOperationException();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the description text for the start/pause button.
+        /// </summary>
+        public string StartPauseButtonDescription
+        {
+            get
+            {
+                switch (this.status)
+                {
+                    case TrackingStatus.Started:
+                        return Symbol.Pause.ToString();
+                    case TrackingStatus.Paused:
+                        return Symbol.Play.ToString();
+                    case TrackingStatus.Stopped:
+                        return Symbol.Play.ToString();
+                    default:
+                        throw new InvalidOperationException();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the icon for the stop button.
+        /// </summary>
+        public Symbol StopButtonIcon => Symbol.Stop;
     }
 }
