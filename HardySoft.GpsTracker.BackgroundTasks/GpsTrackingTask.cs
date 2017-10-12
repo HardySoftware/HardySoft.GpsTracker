2e21a230247e3f17eefab7c76e40a1ccac13920f
Hardy Wang <hardywang@outlook.com>
2017-10-11 22:53:16 -0400

    Create background task, and hook it up with the view model.

---
+++ HardySoft.GpsTracker.BackgroundTasks/GpsTrackingTask.cs
@@ -0,0 +1,22 @@
+﻿namespace HardySoft.GpsTracker.BackgroundTasks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Windows.ApplicationModel.Background;
+
+    /// <summary>
+    /// A background task to perform GPS tracking.
+    /// </summary>
+    public sealed class GpsTrackingTask : IBackgroundTask
+    {
+        /// <inheritdoc />
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            Debug.WriteLine($"{DateTime.Now} - From Gps background task.");
+        }
+    }
+}
--- HardySoft.GpsTracker/ViewModels/DashboardPageViewModel.cs
+++ HardySoft.GpsTracker/ViewModels/DashboardPageViewModel.cs
@@ -1,8 +1,12 @@
 ﻿namespace HardySoft.GpsTracker.ViewModels
 {
     using System;
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.Diagnostics;
+    using System.Threading.Tasks;
     using System.Windows.Input;
+    using HardySoft.GpsTracker.BackgroundTasks;
     using HardySoft.GpsTracker.Models;
     using HardySoft.GpsTracker.Services.Gpx;
     using HardySoft.GpsTracker.Services.Location;
@@ -10,8 +14,13 @@
     using Prism.Commands;
     using Prism.Windows.AppModel;
     using Prism.Windows.Mvvm;
+    using Prism.Windows.Navigation;
+    using Windows.ApplicationModel.Background;
     using Windows.ApplicationModel.Core;
+    using Windows.Devices.Geolocation;
+    using Windows.Storage;
     using Windows.UI.Core;
+    using Windows.UI.Xaml;
     using Windows.UI.Xaml.Controls;
 
     /// <summary>
@@ -20,14 +29,14 @@
     public class DashboardPageViewModel : ViewModelBase
     {
         /// <summary>
+        /// The background task name.
+        /// </summary>
+        private const string BackgroundTaskName = "GPS_Tracking_Task";
+
+        /// <summary>
         /// A GPX handler implementation.
         /// </summary>
         private readonly IGpxHandler gpxHandler;
-
-        /// <summary>
-        /// A location tracker implementation.
-        /// </summary>
-        private readonly ILocationTracker locationTracker;
 
         /// <summary>
         /// The status of the tracking.
@@ -45,21 +54,29 @@
         private string coordinateInformation;
 
         /// <summary>
+        /// A timer used to refresh the information on screen periodically.
+        /// </summary>
+        private DispatcherTimer refreshTimer;
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="DashboardPageViewModel"/> class.
         /// </summary>
         /// <param name="gpxHandler">The Gpx handler implementation it depends on.</param>
-        /// <param name="locationTracker">The location tracker implementation it depends on.</param>
-        public DashboardPageViewModel(IGpxHandler gpxHandler, ILocationTracker locationTracker)
+        public DashboardPageViewModel(IGpxHandler gpxHandler)
         {
             this.gpxHandler = gpxHandler ?? throw new ArgumentNullException(nameof(gpxHandler));
-            this.locationTracker = locationTracker ?? throw new ArgumentNullException(nameof(locationTracker));
 
             this.status = TrackingStatus.Stopped;
             this.StartPauseClickedCommand = new DelegateCommand<ItemClickEventArgs>(this.OnStartPauseClicked, this.CanStartPauseClick);
             this.SelectedActivity = ActivityTypes.Unknown;
             this.CoordinateInformation = "Your location information";
 
-            this.locationTracker.OnTrackingProgressChangedEvent += this.LocationTracker_OnTrackingProgressChangedEvent;
+            this.refreshTimer = new DispatcherTimer()
+            {
+                Interval = new TimeSpan(0, 0, 10)
+            };
+
+            this.refreshTimer.Tick += async (object sender, object e) => { await this.DisplayMostRecentLocationData(string.Empty); };
         }
 
         /// <summary>
@@ -154,18 +171,53 @@
             }
         }
 
+        /// <inheritdoc />
+        public override void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+        {
+            // Check if the background task is active, the "IsBackgroundTaskActive" state is set by the background task.
+            bool isBackgroundTaskActive =
+                ApplicationData.Current.LocalSettings.Values.ContainsKey("IsBackgroundTaskActive") &&
+                (bool)ApplicationData.Current.LocalSettings.Values["IsBackgroundTaskActive"];
+
+            if (isBackgroundTaskActive)
+            {
+                this.refreshTimer.Start();
+            }
+
+            base.OnNavigatedTo(e, viewModelState);
+        }
+
+        /// <inheritdoc />
+        public override void OnNavigatingFrom(NavigatingFromEventArgs e, Dictionary<string, object> viewModelState, bool suspending)
+        {
+            Window.Current.VisibilityChanged -= new WindowVisibilityChangedEventHandler(this.VisibilityChanged);
+            ApplicationData.Current.LocalSettings.Values["IsAppVisible"] = false;
+
+            base.OnNavigatingFrom(e, viewModelState, suspending);
+        }
+
         /// <summary>
         /// Handle start/pause button clicked event.
         /// </summary>
         /// <param name="argument">The event argument.</param>
         private async void OnStartPauseClicked(ItemClickEventArgs argument)
         {
-            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-            {
-                this.CoordinateInformation = "In progress.";
-            });
+            Window.Current.VisibilityChanged += new WindowVisibilityChangedEventHandler(this.VisibilityChanged);
 
-            await this.locationTracker.StartTrack(5, 60);
+            // RequestAccessAsync must be called on the UI thread.
+            var backGroundAccessStatus = await BackgroundExecutionManager.RequestAccessAsync();
+            if (backGroundAccessStatus == BackgroundAccessStatus.AlwaysAllowed || backGroundAccessStatus == BackgroundAccessStatus.AllowedSubjectToSystemPolicy)
+            {
+                var locationAccessStatus = await Geolocator.RequestAccessAsync();
+
+                if (locationAccessStatus == GeolocationAccessStatus.Allowed)
+                {
+                    this.StartBackgroundTask();
+                    Debug.WriteLine($"{DateTime.Now} - Background task started");
+                    this.refreshTimer.Start();
+                    await this.DisplayMostRecentLocationData("In progress.");
+                }
+            }
         }
 
         /// <summary>
@@ -180,20 +232,75 @@
         }
 
         /// <summary>
-        /// An event handler to handle GPS track changed event.
+        /// This is the event handler for VisibilityChanged events.
         /// </summary>
-        /// <param name="statusUpdate">The event argument with detailed data.</param>
-        private async void LocationTracker_OnTrackingProgressChangedEvent(LocationResponse statusUpdate)
+        /// <param name="sender">The event sender object.</param>
+        /// <param name="e">Event data that can be examined for the current visibility state.</param>
+        private void VisibilityChanged(object sender, VisibilityChangedEventArgs e)
+        {
+            ApplicationData.Current.LocalSettings.Values["IsAppVisible"] = e.Visible;
+
+            if (e.Visible)
+            {
+                this.refreshTimer.Start();
+            }
+            else
+            {
+                this.refreshTimer.Stop();
+            }
+        }
+
+        /// <summary>
+        /// Starts the background task.
+        /// </summary>
+        private void StartBackgroundTask()
+        {
+            // Make sure only 1 task is running.
+            this.FindAndCancelExistingBackgroundTask();
+
+            // Register the background task.
+            var backgroundTaskBuilder = new BackgroundTaskBuilder()
+            {
+                TaskEntryPoint = typeof(GpsTrackingTask).FullName,
+                Name = BackgroundTaskName,
+            };
+
+            backgroundTaskBuilder.SetTrigger(new TimeTrigger(15, false));
+            backgroundTaskBuilder.Register();
+        }
+
+        /// <summary>
+        /// Finds a previously registered background task and cancels it (if present)
+        /// </summary>
+        private void FindAndCancelExistingBackgroundTask()
+        {
+            foreach (var backgroundTask in BackgroundTaskRegistration.AllTasks.Values)
+            {
+                if (backgroundTask.Name == BackgroundTaskName)
+                {
+                    ((BackgroundTaskRegistration)backgroundTask).Unregister(true);
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets most recent location data from background task and display on screen.
+        /// </summary>
+        /// <param name="message">The message to display instead of the location data.</param>
+        /// <returns>The asynchronous task object.</returns>
+        private async Task DisplayMostRecentLocationData(string message)
         {
             await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
             {
-                if (statusUpdate.Coordinate != null)
+                if (!string.IsNullOrWhiteSpace(message))
                 {
-                    this.CoordinateInformation = statusUpdate.Coordinate.Point.Position.Latitude.ToString() + ", " + statusUpdate.Coordinate.Point.Position.Longitude.ToString();
+                    this.CoordinateInformation = message;
                 }
                 else
                 {
-                    this.CoordinateInformation = "Getting your location now, please be patient.";
+                    // TODO implement the logic.
+                    this.CoordinateInformation = string.Empty;
                 }
             });
         }
